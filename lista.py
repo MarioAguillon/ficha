a351543b4062fb5f935973c56d5e3f69a048dcd3
MarioAguillon <aguillondavis@gmail.com>
2025-08-25 17:16:14 -0500

    Create lista.py

---
+++ lista.py
@@ -0,0 +1,7 @@
+listas = ["Ivan", "Luis", "Jose", "Diego", 25]
+tuplas = ("Ivan", "Luis", "Jose", "Diego",25)
+diccionario = {"Nombre": "Ivan", "apellido": "Malaver", "Edad": 47}
+
+print (listas [2])
+print (tuplas [2])
+print (diccionario ["apellido"])
