900307b45e8025c769f36c3727833dfb86e08c0b
MarioAguillon <aguillondavis@gmail.com>
2025-08-25 16:21:34 -0500

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,46 @@
+varA = 1
+varB = 1
+varC = 1
+varD = 1
+varE = 1
+varF = 1
+varG = 0
+
+if varA == 0 and varB == 0 and varC == 0 and varD == 0 and varE == 1 and varF == 1 and varG == 0:
+    print ("este numero es el 1")
+
+if varA == 1 and varB == 1 and varC == 0 and varD == 1 and varE == 1 and varF == 0 and varG == 1:
+    print ("este numero es el 2")
+    
+if varA == 1 and varB == 1 and varC == 1 and varD == 1 and varE == 0 and varF == 0 and varG == 1:
+    print ("este numero es el 3")
+
+if varA == 0 and varB == 1 and varC == 1 and varD == 0 and varE == 0 and varF == 1 and varG == 1:
+    print ("este numero es el 4")
+
+if varA == 1 and varB == 0 and varC == 1 and varD == 1 and varE == 0 and varF == 0 and varG == 1:
+    print ("este numero es el 5")
+
+if varA == 1 and varB == 0 and varC == 1 and varD == 1 and varE == 1 and varF == 1 and varG == 1:
+    print ("este numero es el 6")
+
+if varA == 1 and varB == 1 and varC == 0 and varD == 0 and varE == 0 and varF == 0 and varG == 0:
+    print ("este numero es el 7")
+
+if varA == 1 and varB == 1 and varC == 1 and varD == 1 and varE == 1 and varF == 1 and varG == 1:
+    print ("este numero es el 8")
+
+if varA == 1 and varB == 1 and varC == 1 and varD == 1 and varE == 1 and varF == 1 and varG == 1:
+    print ("este numero es el 9")
+
+if varA == 1 and varB == 1 and varC == 1 and varD == 1 and varE == 1 and varF == 1 and varG == 0:
+    print ("este numero es el 0")
+
+else:
+    print ("valor desconocido")
+
+
+
+
+
+
